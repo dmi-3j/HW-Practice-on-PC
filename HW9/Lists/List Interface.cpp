af4b8ebf4628236ebb8fa00fa061719be537be50
Dmitry <st076480@student.spbu.ru>
2019-12-18 19:14:19 +0300

    Update

--- HW9/Lists/List Interface.cpp
+++ HW9/Lists/List Interface.cpp
@@ -3,15 +3,14 @@
 #include <malloc.h>
 #include "list_library.h"
 
-const int ACTION01 = 1;
-const int ACTION02 = 2;
-const int ACTION03 = 3;
-const int ACTION04 = 4;
-const int ACTION05 = 5;
-const int ACTION06 = 6;
-const int ACTION07 = 7;
-const int ACTION08 = 8;
-const int ACTION09 = 9;
+const int printList = 1;
+const int listCreate = 2;
+const int addHead = 3;
+const int addTail = 4;
+const int addByValue = 5;
+const int deleteElement = 6;
+const int cleanList = 7;
+const int exitWithClean = 9;
 int main()
 {
 
@@ -33,85 +32,84 @@ int main()
 		printf("\t\nEnter your choice: ");
 		scanf_s("%d", &choice);
 		switch (choice) {
-		case ACTION01:
-			if (list != NULL) {
-				print(list);
-				printf("\n");
-			}
-			else {
-				printf("\tList is NULL!\n\n");
+			case printList:
+				if (list != NULL) {
+					print(list);
+					printf("\n");
+				}
+				else {
+					printf("\tList is NULL!\n\n");
+					break;
+				}
 				break;
-			}
-			break;
-		case ACTION02:
-			list = createList();
-			printf("\tList was created\n\n\n");
-			break;
-		case ACTION03:
-			if (list != NULL) {
-				printf("\n\tYour value: ");
-				scanf_s("%d", &value);
-				addToHead(list, value);
-			}
-			else {
-				printf("List is NULL!\n\n");
+			case listCreate:
+				list = createList();
+				printf("\tList was created\n\n\n");
 				break;
-			}
-			break;
-		case ACTION04:
-			if (list != NULL) {
-				printf("\tYour value: ");
-				scanf_s("%d", &value);
-				addToTail(list, value);
-			}
-			else {
-				printf("List is NULL!\n\n");
+			case addHead:
+				if (list != NULL) {
+					printf("\n\tYour value: ");
+					scanf_s("%d", &value);
+					addToHead(list, value);
+				}
+				else {
+					printf("List is NULL!\n\n");
+					break;
+				}
 				break;
-			}
-			break;
-		case ACTION05:
-			if (list != NULL) {
-				printf("\tAfter element with value: ");
-				scanf_s("%d", &after);
-				printf("\n\tAdd value: ");
-				scanf_s("%d", &value);
-				addAfterFromValue(list, after, value);
-			}
-			else {
-				printf("List is NULL!\n\n");
+			case addTail:
+				if (list != NULL) {
+					printf("\tYour value: ");
+					scanf_s("%d", &value);
+					addToTail(list, value);
+				}
+				else {
+					printf("List is NULL!\n\n");
+					break;
+				}
 				break;
-			}
-			break;
-		case ACTION06:
-			if (list != NULL) {
-				printf("\n\tYour value: ");
-				scanf_s("%d", &value);
-				deleteNodeByValue(list, value);
-			}
-			else {
-				printf("List is NULL!\n\n");
+			case addByValue:
+				if (list != NULL) {
+					printf("\tAfter element with value: ");
+					scanf_s("%d", &after);
+					printf("\n\tAdd value: ");
+					scanf_s("%d", &value);
+					addAfterFromValue(list, after, value);
+				}
+				else {
+					printf("List is NULL!\n\n");
+					break;
+				}
 				break;
-			}
-			break;
-		case ACTION07:
-			if (list != NULL) {
-				clean(list);
-				printf("\tlist was cleaned\n\n");
-			}
-			else {
-				printf("List is NULL!\n\n");
+			case deleteElement:
+				if (list != NULL) {
+					printf("\n\tYour value: ");
+					scanf_s("%d", &value);
+					deleteNodeByValue(list, value);
+				}
+				else {
+					printf("List is NULL!\n\n");
+					break;
+				}
 				break;
-			}
-			break;
-		case ACTION09:
-			free(list);
-			exit(0); 
-			break;
-		default:
-			printf("\tOption not listed\n\n");
-			break;
+			case cleanList:
+				if (list != NULL) {
+					clean(list);
+					printf("\tlist was cleaned\n\n");
+				}
+				else {
+					printf("List is NULL!\n\n");
+					break;
+				}
+				break;
+			case exitWithClean:
+				free(list);
+				exit(0); 
+				break;
+			default:
+				printf("\tOption not listed\n\n");
+				break;
 		}
-		free(list);													//Не будет утечки памяти? В 9 пункте при выходе лист очищается
 	}
 	return 0;
 }
